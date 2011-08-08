411f08a212c009654cf48bfb892b1a4fa4841bee
olim7t <omichallat+github@gmail.com>
2011-08-08 19:49:06 +0200

    Add local knife config

---
+++ .chef/knife.rb
@@ -0,0 +1,10 @@
+log_level                :info
+log_location             STDOUT
+node_name                'olim7t'
+client_key               '/home/olim7t/chef-repo/.chef/olim7t.pem'
+validation_client_name   'chef-validator'
+validation_key           '/etc/chef/validation.pem'
+chef_server_url          'none'
+cache_type               'BasicFile'
+cache_options( :path => '/home/olim7t/chef-repo/.chef/checksums' )
+cookbook_path [ './cookbooks', './site-cookbooks' ]
