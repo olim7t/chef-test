31c8319a0b2bd27672e7a6fbf559e01da98712ef
olim7t <omichallat+github@gmail.com>
2011-08-15 19:11:15 +0200

    Make Sonar db creation idempotent.

--- cookbooks/sonar/recipes/database_mysql.rb
+++ cookbooks/sonar/recipes/database_mysql.rb
@@ -1,9 +1,12 @@
 include_recipe "mysql::server"
 
+database_already_exists = "test -d #{node[:mysql][:data_dir]}/sonar"
+
 # Setup sonar user
 grants_path = "/opt/sonar/extras/database/mysql/create_database.sql"
 
 template "/opt/sonar/extras/database/mysql/create_database.sql" do
+  not_if database_already_exists
   path grants_path
   source "create_mysql_database.sql.erb"
   owner "root"
@@ -13,6 +16,7 @@ template "/opt/sonar/extras/database/mysql/create_database.sql" do
 end
 
 execute "mysql-install-application-privileges" do
+  not_if database_already_exists
   command "/usr/bin/mysql -u root #{node[:mysql][:server_root_password].empty? ? '' : '-p' }#{node[:mysql][:server_root_password]} < #{grants_path}"
 end
 
